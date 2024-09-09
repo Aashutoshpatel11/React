d4bd7e7910b18aab524dabfbe0a335a01b3137a9
Aashutosh Patel <aashutoshpatel1110@gmail.com>
2024-09-09 19:02:56 +0530

    basic react app

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Aashutosh Patel | Learning React</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Aashutosh Patel | Learning React + Vite</h1>
+  )
+}
+
+export default App
