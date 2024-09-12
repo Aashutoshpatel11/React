68082b0f98ccf64758f5dc9f8153d43dcb36a4b1
Aashutosh Patel <aashutoshpatel1110@gmail.com>
2024-09-12 21:08:04 +0530

    Learned about HOOKS

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter , setcounter] = useState(10)
+
+  let ADD = ()=>{
+    if (counter<20) setcounter(counter+1)
+  }
+  let DELETE = ()=>{
+    if(counter>0) setcounter(counter-1)
+  }
+
+  return (
+    <>
+    <h1>Aashutosh Patel</h1>
+    <h2>Count : {counter}</h2>
+
+    <button
+    onClick={ADD}
+    >ADD</button>
+    <br/>
+    <button
+    onClick={DELETE}
+    >DELETE</button>
+    <p>{counter}</p>
+    </>
+  )
+}
+
+export default App
