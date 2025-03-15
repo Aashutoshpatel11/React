f5be26863e6b3e1694a979b8ecf910357f22af64
Aashutosh Patel <aashutoshpatel1110@gmail.com>
2025-03-15 17:08:20 +0530

    Understood adding components in react.

---
+++ 01basicreact/src/temp.js
@@ -0,0 +1,11 @@
+
+
+function temp() {
+    return(
+        <>
+        <h1>TEMP component of react</h1>
+        </>
+    )
+}
+
+export default temp;
---
+++ 02vitereact/src/temp.jsx
@@ -0,0 +1,11 @@
+
+
+function temp(){
+    return (
+        <>
+        <h1>TEMP component render</h1>
+        </>
+    )
+}
+
+export default temp;
